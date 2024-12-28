dc11a075bceb7c4a05e77a1f06a23a97ba834b49
unknown <evandriridho321@gmail.com>
2024-12-28 19:01:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,95 @@
 // Import the createServer function
-const { createServer } = require('node:http');
+const { createServer, STATUS_CODES } = require('node:http');
+const { randomUUID } = require('node:crypto');
 const server = createServer();
 
-const HomePage = `
-        <!DOCTYPE html>
-            <html lang="en">
-                <head>        
-                    <meta charset="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Document</title>
-                </head>
-                <body>
-                    <h1>HomePage</h1>
-                </body>
-            </html>
-    `
+const tasks = [
+    {
+        id: randomUUID(),
+        title: 'Belajar NodeJS',
+        status: 'onprogress'
+    },
+    {
+        id: randomUUID(),
+        title: 'Belajar ReactJS',
+        status: 'completed'
+    },
+    {
+        id: randomUUID(),
+        title: 'Belajar NextJS',
+        status: 'todo'
+    },
+]
 
-const AboutPage = `
-        <!DOCTYPE html>
-            <html lang="en">
-                <head>        
-                    <meta charset="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Document</title>
-                </head>
-                <body>
-                    <h1>AboutPage</h1>
-                </body>
-            </html>
-    `
-
-const NotFoundPage = `
-        <!DOCTYPE html>
-            <html lang="en">
-                <head>        
-                    <meta charset="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Document</title>
-                </head>
-                <body>
-                    <h1>NotFoundPage</h1>
-                </body>
-            </html>
-    `
-
-
+// Membuat Request
 server.on("request", (req, res) => {
     const { method, url, headers } = req
-    console.debug(method, url, headers.host)
+    console.debug(method, url, headers.date);
 
+    // Routing
     switch (url) {
-        case '/home':
-            res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': HomePage.length });
-            res.end(HomePage);
-            return;
-        case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': AboutPage.length });
-            res.end(AboutPage);
-            return;
+        case '/api/v1/tasks':
+            switch (method) {
+                case 'GET':
+                    const jsonRaw = JSON.stringify(tasks);
+                    res.writeHead(200, { 'Content-Type': 'application/json', 'content-length': jsonRaw.length });
+                    res.write(jsonRaw);
+                    res.end();
+                    return;
+                case 'POST':
+                    res.writeHead(501, { 'Content-Type': 'application/json' });
+                    res.end();
+                    return;
+                case 'DELETE':
+                    res.writeHead(501, { 'Content-Type': 'application/json' });
+                    res.end();
+                    return;
+                case 'PUT':
+                    res.writeHead(501, { 'Content-Type': 'application/json' });
+                    res.end();
+                    return;
+                default:
+                    res.writeHead(405, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ message: STATUS_CODES[405] }));
+                    return;
+            }
         default:
-            res.writeHead(404, { 'Content-Type': 'text/html', 'Content-Length': NotFoundPage.length });
-            res.end(NotFoundPage);
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: STATUS_CODES[404] }));
     }
 })
 
+// Membuat Connection
 server.on("connection", (socket) => {
     const { remoteAddress, remotePort } = socket;
     console.log(`Connection from ${remoteAddress}:${remotePort}`);
 })
 
+// Membuat Close
+server.on("close", () => {
+    console.log("Server closed successfully!!!");
+    process.exit(0);
+})
+
+// Membuat Error
+server.on("error", (error) => {
+    console.error(error);
+})
+
+// Membuat Signal
+process.on('SIGINT', handleShoutdown);
+process.on('SIGTERM', handleShoutdown);
+
+function handleShoutdown(signal) {
+    console.log('Shutting down server started', { signal });
+    server.close((error) => {
+        console.log('Cannot Close Server', { error });
+    });
+    console.log('Shutting down server completed', { signal });
+}
+
 // Membuat Port
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 server.listen(port, 'localhost', () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 })
